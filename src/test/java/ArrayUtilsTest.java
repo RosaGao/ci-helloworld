573b8db3bfea2fcd4612ea0490c55e589a607141
Rosa <ygao112@jhu.edu>
2024-03-28 15:05:27 -0400

    restore coverage again

--- src/test/java/ArrayUtilsTest.java
+++ src/test/java/ArrayUtilsTest.java
@@ -22,16 +22,16 @@ public class ArrayUtilsTest {
     assertEquals(-1, ArrayUtils.findLast(new int[]{1, 2}, 3));
   }
 
-//  @Test
-//  public void testFindLastExistsFirstElement() {
-//    assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
-//  }
-//
-//  @Test
-//  public void testOddOrPosNull() {
-//    // Do not execute fault
-//    assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
-//  }
+  @Test
+  public void testFindLastExistsFirstElement() {
+    assertEquals(0, ArrayUtils.findLast(new int[]{2, 3, 5}, 2));
+  }
+
+  @Test
+  public void testOddOrPosNull() {
+    // Do not execute fault
+    assertThrows(NullPointerException.class, () -> {ArrayUtils.oddOrPos(null);});
+  }
 
 
   @Test
